488f7636fcf72bf867ba7fa2c56097242ad58c0e
marijmokoginta <marijmokoginta04@gmail.com>
2023-01-04 10:43:53 +0800

    update

--- app/src/main/java/com/mmacodes/antiphishing/AnalyzeResult.java
+++ app/src/main/java/com/mmacodes/antiphishing/AnalyzeResult.java
@@ -2,6 +2,7 @@ package com.mmacodes.antiphishing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import com.mmacodes.antiphishing.model.ipqs.PhishingAnalyze;
 public class AnalyzeResult extends AppCompatActivity {
 
     private TextView riskScore, urlScanned, spamming, malware, phishing, suspicious, adult, category, domainAge;
+    private PhishingAnalyze phishingAnalyze;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,7 @@ public class AnalyzeResult extends AppCompatActivity {
         initView();
 
         Intent getAnalyzeResult = getIntent();
-        PhishingAnalyze phishingAnalyze = getAnalyzeResult.getParcelableExtra("ANALYZE_RESULT");
+        phishingAnalyze = getAnalyzeResult.getParcelableExtra("ANALYZE_RESULT");
         String url = getAnalyzeResult.getStringExtra("URL_SCANNED");
 
         riskScore.setText(String.valueOf(phishingAnalyze.getRisk_score()));
@@ -32,6 +34,51 @@ public class AnalyzeResult extends AppCompatActivity {
         adult.setText(phishingAnalyze.isAdult() ? R.string.yes : R.string.no);
         category.setText(phishingAnalyze.getCategory());
         domainAge.setText(phishingAnalyze.getDomain_age().getHuman());
+
+        setColor();
+    }
+
+    @SuppressLint({"UseCompatLoadingForDrawables", "UseCompatTextViewDrawableApis"})
+    private void setColor() {
+        if(phishingAnalyze.isSpamming()) {
+            spamming.setTextColor(getResources().getColor(R.color.danger, getTheme()));
+            spamming.setCompoundDrawables(getResources().getDrawable(R.drawable.ic_warning, getTheme()), null, null, null);
+            spamming.setCompoundDrawableTintList(getColorStateList(R.color.danger));
+        } else {
+            spamming.setTextColor(getResources().getColor(R.color.success, getTheme()));
+        }
+
+        if(phishingAnalyze.isMalware()) {
+            malware.setTextColor(getResources().getColor(R.color.danger, getTheme()));
+            malware.setCompoundDrawables(getResources().getDrawable(R.drawable.ic_warning, getTheme()), null, null, null);
+            malware.setCompoundDrawableTintList(getColorStateList(R.color.danger));
+        } else {
+            malware.setTextColor(getResources().getColor(R.color.success, getTheme()));
+        }
+
+        if(phishingAnalyze.isPhishing()) {
+            phishing.setTextColor(getResources().getColor(R.color.danger, getTheme()));
+            phishing.setCompoundDrawables(getResources().getDrawable(R.drawable.ic_warning, getTheme()), null, null, null);
+            phishing.setCompoundDrawableTintList(getColorStateList(R.color.danger));
+        } else {
+            phishing.setTextColor(getResources().getColor(R.color.success, getTheme()));
+        }
+
+        if(phishingAnalyze.isSuspicious()) {
+            suspicious.setTextColor(getResources().getColor(R.color.danger, getTheme()));
+            suspicious.setCompoundDrawables(getResources().getDrawable(R.drawable.ic_warning, getTheme()), null, null, null);
+            suspicious.setCompoundDrawableTintList(getColorStateList(R.color.danger));
+        } else {
+            suspicious.setTextColor(getResources().getColor(R.color.success, getTheme()));
+        }
+
+        if(phishingAnalyze.isAdult()) {
+            adult.setTextColor(getResources().getColor(R.color.danger, getTheme()));
+            adult.setCompoundDrawables(getResources().getDrawable(R.drawable.ic_warning, getTheme()), null, null, null);
+            adult.setCompoundDrawableTintList(getColorStateList(R.color.danger));
+        } else {
+            adult.setTextColor(getResources().getColor(R.color.success, getTheme()));
+        }
     }
 
     private void initView() {
